d5e9cc3e054a1324aaa95b20b5541468813fce47
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-10-30 17:57:26 -0400

    It cannot read the page for some reason

--- apple_stock.py
+++ apple_stock.py
@@ -1,4 +1,17 @@
 from bs4 import BeautifulSoup
+import requests
+
+def getHTMLdocument(url):
+    response = requests.get(url)
+    return response.text
+
+url_to_scrape = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+stock_data = getHTMLdocument(url_to_scrape)
+
+soup = BeautifulSoup(open(stock_data),
+                      "lxml")
+
+print(soup.prettify())
 
 if __name__ == "__main__":
     pass
