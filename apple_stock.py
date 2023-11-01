d5422c7db122bc785cbc85d88698fbe8f611bcea
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-11-01 18:42:53 -0400

    Challenging, but somewhat manageable.

--- apple_stock.py
+++ apple_stock.py
@@ -21,6 +21,27 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(historic_data, features="lxml")
 
-    stock_data = soup.find_all('table', class_= "W(100%) M(0)")
+    stock_data = soup.find_all('table', class_="W(100%) M(0)")
 
     print(stock_data)
+
+    #This line read the rows, if the bracket with a number is present, it indicates the row to mark, and the colon determines the cells to ignore, before or after#
+    rows = stock_data[0].find_all('tr')
+
+    # This selects the headers
+    headers = stock_data[0].find_all('th')
+
+    print(
+        f"{headers[0].text.strip()} - {headers[1].text.strip()} - "
+        f"{headers[2].text.strip()} - {headers[3].text.strip()} - {headers[4].text.strip()} - "
+        f"{headers[5].text.strip()} - {headers[6].text.strip()}")
+
+    for row in rows:
+        cells = row.find_all('td')
+        if not cells:
+            continue
+        print(
+            f"{cells[0].text.strip():<15} - {cells[1].text.strip():<25} - "
+            f"{cells[2].text.strip():<15} - {cells[3].text.strip():<15} - "
+            f"{cells[4].text.strip():<15} - {cells[5].text.strip():<15} - "
+            f"{cells[6].text.strip():<15}")
--- football_stats.py
+++ football_stats.py
@@ -20,8 +20,27 @@ if __name__ == "__main__":
     scoring_data = download(football_url)
 
     soup = BeautifulSoup(scoring_data, features="lxml")
+
     touchdown_data = soup.find_all('div', class_="TableBase")
 
-    print(touchdown_data)
+    #This line read the rows, if the bracket with a number is present, it indicates the row to mark, and the colon determines the cells to ignore, before or after#
+    rows = touchdown_data[0].find_all('tr')[:21]
 
+    #This selects the headers
+    headers = touchdown_data[0].find_all('th')
+
+    print(
+        f"{headers[0].text.strip()} - {headers[1].text.strip()} - "
+        f"{headers[2].text.strip()} - {headers[3].text.strip()} - {headers[4].text.strip()} - "
+        f"{headers[5].text.strip()} - {headers[6].text.strip()} - {headers[7].text.strip()}")
+
+    for row in rows:
+        cells = row.find_all('td')
+        if not cells:
+            continue
+        print(
+            f"{cells[0].text.strip():<15} - {cells[1].text.strip():<25} - "
+            f"{cells[2].text.strip():<15} - {cells[3].text.strip():<15} - "
+            f"{cells[4].text.strip():<15} - {cells[5].text.strip():<15} - "
+            f"{cells[6].text.strip():<15} - {cells[7].text.strip():<15}")
 
